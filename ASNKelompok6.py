60cfd0a9b13cb4d9ea228b95047fadcd93bdd07c
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-06-05 13:45:12 +0700

    Add files via upload

---
+++ ASNKelompok6.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+import math
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+column_names = ['ECG']
+data=pd.read_csv('ECG5minutes.txt',delimiter="\t", names=column_names)
+data["sample interval"] = np.arange(len(data))
+data["elapsed time"] = (data["sample interval"])*(1/200)
+x=data["elapsed time"]
+y=data["ECG" ] - (sum(data["ECG" ]/len(data["ECG"]))) #agar turun ke baseline
+
+
+
+with st.sidebar:
+    selected = option_menu("TUGAS 1", ["Home","Ecyclopedia", "Data & Graphic", "Filter","Method","HRV Analysis"], icons=['house', 'book',None,None,None,None], menu_icon="cast", default_index=1)
+
+if selected == "Home":
+   st.title('Project ASN Kelompok 6')
+   st.subheader("Anggota kelompok")
+   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">Farhan Majid Ibrahim - 5023211049</p>'
+   st.markdown(new_title, unsafe_allow_html=True)
+   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">Nayla Pramudhita Putri Pertama - 5023211012</p>'
+   st.markdown(new_title, unsafe_allow_html=True)
+   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">Mohammad Rayhan Amirul Haq Siregar - 5023211045</p>'
+   st.markdown(new_title, unsafe_allow_html=True)
+   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">Reynard Prastya Savero - 5023211042</p>'
+   st.markdown(new_title, unsafe_allow_html=True)
+
+
+
